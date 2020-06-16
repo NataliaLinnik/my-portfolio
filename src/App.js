f708c809695e88e8b79de42884b1b5f05c1f7326
Natalia Linnik <natali.linnik24@gmail.com>
2020-06-16 12:13:55 +0200

    Add styled components and remove sass + add babel config.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.scss";
+import "./App.css";
+import styled from "styled-components";
+
+let styles = {};
+
+styles.Title = styled.div`
+  font-size: 4rem;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <styles.Title>About me</styles.Title>
+        <styles.Title>Education</styles.Title>
+        <styles.Title>Work expirience</styles.Title>
+        <styles.Title>Skills</styles.Title>
+        <styles.Title>Hobbies</styles.Title>
+        <styles.Title>Let's talk</styles.Title>
       </header>
     </div>
   );
